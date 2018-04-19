25b5064efc939f9750413edb5a2060cf3cfefe02
lfermin77 <leonardo.fermin@gmail.com>
2018-04-19 15:49:57 +0200

    all files in optimal_weights.mat

--- plot_all_graphs.m
+++ plot_all_graphs.m
@@ -7,11 +7,12 @@ f = figure('visible','off');
 for counter=1:length(weights_in_graph)
     G=graph(edges_in_graph{counter}(:,1), edges_in_graph{counter}(:,2), weights_in_graph{counter});
     figure(1)
-    LWidths = 5*G.Edges.Weight/max(G.Edges.Weight);
-    fig=plot(G,'LineWidth',LWidths);
+
     counter
     filename=sprintf('images/Graph_n%d_crit%c',counter, 'E')
-    if (counter >135)
+    if (counter >0)
+        LWidths = 5*G.Edges.Weight/max(G.Edges.Weight);
+        fig=plot(G,'LineWidth',LWidths);
         saveas(fig, filename, 'epsc')
         pause(1.5)
     end
